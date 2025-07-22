909ba67bdd366de9418667ece3ec5fc2286d923c
Teenasri <teenasreekasala@gmail.com>
2025-07-22 21:03:16 +0530

    Initial commit

---
+++ intro.py
@@ -0,0 +1,10 @@
+Name='K.Teena sri!'
+Rollno='24BFA32080$'
+Subject='Python programming😊'
+College='srivenkateswaracollege'
+Department='C😊S😊D'
+print("name:",Name)
+print("Rollno:",Rollno)
+print("Subject:",Subject)
+print("College:",College)
+print("Department:",Department)
