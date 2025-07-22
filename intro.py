5e26542198536d48bf4f694b95f25365f0ef508e
Teenasri <teenasreekasala@gmail.com>
2025-07-22 21:22:24 +0530

    modifications done

--- intro.py
+++ intro.py
@@ -1,10 +1,11 @@
-Name='K.Teena sri!'
+Name='K.Teena sree!'
 Rollno='24BFA32080$'
 Subject='Python programming😊'
-College='srivenkateswaracollege'
+College='srivenkateswaracollegeofengineering'
 Department='C😊S😊D'
-print("name:",Name)
+print("Name:",Name)
 print("Rollno:",Rollno)
 print("Subject:",Subject)
 print("College:",College)
 print("Department:",Department)
+print("Hello svce")
